7f5c7ae6d925480f94491401fcc2eaa6830309ef
Jade McDaniels <jademcdaniels81@gmail.com>
2020-12-29 21:11:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,7 @@ def adding_matz
       }
     }
    programmer_hash[:yokihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
-   programmer_hash
+   return programmer_hash
 end
 
 def changing_alan
